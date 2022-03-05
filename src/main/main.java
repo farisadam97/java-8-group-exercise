be35c5561179e331719f3ae8740af1d2730d9805
Faris Adam <farisadam97@gmail.com>
2022-03-05 15:21:20 +0700

    add interface, lambda, and method reference

--- src/main/main.java
+++ src/main/main.java
@@ -1,7 +1,33 @@
 package main;
 
+import java.util.function.Function;
+
 public class main {
+    @FunctionalInterface
+    interface booleanOddOrEven{
+        boolean oddOrEven(int a);
+    }
+
+    public boolean OddOrEvenNumber(int a, booleanOddOrEven booleanOddOrEven){
+        return booleanOddOrEven.oddOrEven(a);
+    }
+
+    public boolean testingOddOrEven(int x){
+        return OddOrEvenNumber(x,(a) ->a % 2==0 ? true : false);
+    }
+
     public static void main(String[] args) {
+
+//        Create Interface and using lambda
+        main mainA = new main();
+        System.out.println(mainA.testingOddOrEven(5));
+
+//        Using Method reference
+        Function<String,String> toLowerCase = String::toLowerCase;
+        String testText = toLowerCase.apply("TesTLowErCaSEtHisText");
+        System.out.println(testText);
+
+
 
     }
 }
