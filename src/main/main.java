08ea4fd78558f9c02deafa3e5a1dc582fd654ced
Eva Rahmadanti <eva.rahmadanti@mitrais.com>
2022-03-05 14:38:05 +0700

    adding the filter

--- src/main/main.java
+++ src/main/main.java
@@ -2,10 +2,17 @@ package main;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class main {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 13, 4, 5);
         System.out.println("Input Numbers : " + numbers);
+
+        // Filter data using stream
+        List<Integer> filteredNumber = numbers.stream()
+                .filter(number -> number != 1)
+                .collect(Collectors.toList());
+        System.out.println("Filtered number list : " + filteredNumber);
     }
 }
