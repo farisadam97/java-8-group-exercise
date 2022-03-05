728fa2fda58aecd88ba7fa7c2af5ca1829b73ca9
Arifka Rizki <ArifkaRizki.Fakhriza@mitrais.com>
2022-03-05 15:28:11 +0700

    optional

--- src/main/main.java
+++ src/main/main.java
@@ -1,7 +1,11 @@
 package main;
 
+import java.util.Optional;
+
 public class main {
     public static void main(String[] args) {
-
+        Person p = new Person();
+        String personName = Optional.ofNullable(p.getName()).orElse("No Name");
+        System.out.println(personName);
     }
 }
