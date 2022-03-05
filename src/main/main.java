5eed9acdd8cf9b496e74a912b206c2c01b1b4c3f
Faris Adam <farisadam97@gmail.com>
2022-03-05 16:42:07 +0700

    resolve

--- src/main/main.java
+++ src/main/main.java
@@ -1,33 +1,26 @@
 package main;
 
-import java.time.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Optional;
 
 public class main {
     public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 13, 4, 5);
+        System.out.println("Input Numbers : " + numbers);
 
-        // ADD DATE TIME API IN OUR CODE //
+        // Filter data using stream and mapping data into new structure
+        List<String> filteredNumber = numbers.stream()
+                .map(Object::toString) // mapping integer list into string list
+                .filter(number -> number.startsWith("1")) // filter the list
+                .collect(Collectors.toList());
+        System.out.println("Filtered and mapped numbers list : " + filteredNumber);
+        Integer totalValue = numbers.stream().reduce(0, (a,b) -> a+b);
+        System.out.println("Total Value = " + totalValue);
 
-        System.out.println(LocalDateTime.of(2015, Month.MARCH, 20, 06, 30));
-        System.out.println(LocalDate.now().plusDays(1)); //SHOW TOMORROW DATE
-
-        //Convert to New Date Time API
-        Date tgl = new Date();
-        //System.out.println(tgl);
-        Calendar cln = Calendar.getInstance();
-        //System.out.println(cln.getCalendarType());
-        System.out.println(LocalDateTime.ofInstant(tgl.toInstant(), ZoneId.systemDefault()));
-        System.out.println(LocalDateTime.ofInstant(cln.toInstant(), ZoneId.systemDefault()));
-
-        //Using Period to show total days between two date
-        LocalDate workDate = LocalDate.of(2022, 2, 21);
-        Period p = Period.between(workDate, LocalDate.now());
-        System.out.println(p.getDays()+ " Days");
-
-        //Using Duration to show total duration between two instants
-        LocalTime start = LocalTime.of(7, 23,22);
-        LocalTime end = LocalTime.of(9, 34,13);
-        long wacthTime = Duration.between(start, end).getSeconds();
-        System.out.println(wacthTime + " Seconds");
+        Person p = new Person();
+        String personName = Optional.ofNullable(p.getName()).orElse("No Name");
+        System.out.println("Person Name = " + personName);
     }
 }
