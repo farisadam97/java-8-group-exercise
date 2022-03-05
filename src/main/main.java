6f5b12c5bc500ce3ca90724f953b92173247e3cb
Eva Rahmadanti <eva.rahmadanti@mitrais.com>
2022-03-05 14:30:42 +0700

    adding the integer list as an input

--- src/main/main.java
+++ src/main/main.java
@@ -1,7 +1,11 @@
 package main;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class main {
     public static void main(String[] args) {
-
+        List<Integer> numbers = Arrays.asList(1, 2, 13, 4, 5);
+        System.out.println("Input Numbers : " + numbers);
     }
 }
